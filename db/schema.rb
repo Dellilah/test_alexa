e52fc39d784b773804b806d4710d181cc17a9911
Dellilah <acyganiewicz@gmail.com>
2017-02-26 23:11:51 +0100

    User abilities: many to many

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225203041) do
+ActiveRecord::Schema.define(version: 20170226215553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "abilities", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "abilities_users", id: false, force: :cascade do |t|
+    t.integer "ability_id", null: false
+    t.integer "user_id",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
